3acdef7079a691496e95f216222ef0714a64e579
Tiago Barbosa <tiago@farias.cc>
2023-12-07 12:50:18 -0300

    initial commit

---
+++ CountdownTimer/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Countdown");
+
+var minutes = TimeSpan.FromMinutes(1);
+var timer = TimeSpan.FromSeconds(15);
+var interval = TimeSpan.FromSeconds(1);
+// Console.WriteLine(minutes);
+
+Console.Clear();
+for (var rest = timer; rest > TimeSpan.Zero; rest = rest.Subtract(interval))
+{
+    Console.WriteLine(rest);
+    await Task.Delay(1000);
+    Console.Clear();
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# C# - review - micro apps
+
+> Development 🧑🏻‍💻 
+
+- Repository created to group some "micro-apps", reviewing some topics and practicing the use of c# language. 
